a1c7dd2c702961dd70235de04c47093f6686f252
Hassan Kane <hassan.kane2@gmail.com>
2023-05-03 22:39:25 -0400

    initiallayoutv1

--- script.js
+++ script.js
@@ -28,96 +28,32 @@ function processRestaurants(list) {
     return list[index]
   });
 }
-async function mainEvent() { // the async keyword means we can make API requests
-  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  // const filterButton = document.querySelector('#filter_button');// Add a querySelector that targets your filter button here
-  const loadDataButton = document.querySelector('#data_load');// Add a querySelector that targets your load county data button here
-  const clearDataButton = document.querySelector('#data_clear');// Add a querySelector that clears your load county data button here
-  const generateListButton = document.querySelector('#generate');// Add a querySelector that targets your generate button here
-  const loadAnimation = document.querySelector('#load_animation');
-  loadAnimation.style.display = 'none';
-  generateListButton.classList.add('hidden');
-  const textField = document.querySelector('#resto');
-  const carto = initMap();
-  const storedData = JSON.parse(localStorage.getItem('storedData'));
-  if (storedData?.length > 0) {
-    generateListButton.classList.remove('hidden');
-  }
-
-  //let storedList = [];
-  let currentList = []; // this is "scoped" to the main event function
-
-  /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-
-    // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
-    submitEvent.preventDefault();
-
-    // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
-    console.log('loading data...');
-    loadAnimation.style.display = 'inline-block';
-
-    /*
-      ## GET requests and Javascript
-        We would like to send our GET request so we can control what we do with the results
-        Let's get those form results before sending off our GET request using the Fetch API
-    
-      ## Retrieving information from an API
-        The Fetch API is relatively new,
-        and is much more convenient than previous data handling methods.
-        Here we make a basic GET request to the server using the Fetch method to the county
-    */
-
-    // Basic GET request - this replaces the form Action
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-
-    // This changes the response from the GET into data we can use - an "object"
-    const storedList = await results.json();
-    localStorage.setItem('storedData',JSON.stringify(storedList));
-    
-    /*
-      This array initially contains all 1,000 records from your request,
-      but it will only be defined _after_ the request resolves - any filtering on it before that
-      simply won't work.
-    */
-    loadAnimation.style.display = 'none';
-    console.table(storedList);
-    injectHTML(storedList);
+function initChart(chart){
+  
+  return new Chart(chart, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 3
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
   });
 
-  /**filterButton.addEventListener('click', (event) => {
-    console.log('Clicked FilterButton');
-    const formData = new FormData(mainForm);
-    const formProps = Object.fromEntries(formData);
-    console.log(formProps);
-    const newList = filterList(currentList, formProps.resto);
-    console.log(newList);
-    injectHTML(newList);
-  })**/
-
-  generateListButton.addEventListener('click', (event) => {
-    console.log('generate new list');
-    currentList = processRestaurants(storedData);
-    console.log(currentList);
-    injectHTML(currentList);
-    markerPlace(currentList, carto);
-  })
-
-  textField.addEventListener('input', (event) => {
-    console.log('input', event.target.value);
-    const newList = filterList(currentList, event.target.value);
-    console.log(newList);
-    injectHTML(newList);
-    markerPlace(newList, carto);
-  })
-
-  clearDataButton.addEventListener('click', (event)=>{
-    console.log('clear browser data');
-    localStorage.clear();
-    console.log('localStorage Check', localStorage.getItem("storedData"));
-  })
-
-  //filterList
+}
+async function mainEvent() { // the async keyword means we can make API requests
+const 
 }
 
 function initMap (){
@@ -144,6 +80,7 @@ const {coordinates} = item.geocoded_column_1;
 L.marker([coordinates[1], coordinates[0]]).addTo(map);
 })
 }
+
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
